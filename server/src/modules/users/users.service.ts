3d3f700f630e79c4c0fa61344655e3918549f414
Ruslan Mirets <ruslan.mirets@gmail.com>
2022-02-16 09:46:56 +0300

    User Module

---
+++ server/src/modules/users/users.service.ts
@@ -0,0 +1,21 @@
+import { USER_REPOSITORY } from './../../core/constants/index';
+import { Inject, Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+import { UserDto } from './dto/user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(@Inject(USER_REPOSITORY) private readonly userRepository: typeof User) {}
+
+  async create(dto: UserDto): Promise<User> {
+    return await this.userRepository.create<User>(dto);
+  }
+
+  async findOneByEmail(email: string): Promise<User> {
+    return await this.userRepository.findOne<User>({ where: { email } });
+  }
+
+  async findOneById(id: string): Promise<User> {
+    return await this.userRepository.findOne<User>({ where: { id } });
+  }
+}
