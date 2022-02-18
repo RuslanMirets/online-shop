854cbbafd474d1d71da157da7f8b8ebbb1a147a4
Ruslan Mirets <ruslan.mirets@gmail.com>
2022-02-18 11:10:13 +0300

    Delete Client

--- client/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from 'next';
-import MainLayout from '../layouts/MainLayout';
-
-const Home: NextPage = () => {
-  return <MainLayout>Home page</MainLayout>;
-};
-
-export default Home;
--- client/pages/users/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NextPage } from 'next';
-import React from 'react';
-import MainLayout from '../../layouts/MainLayout';
-
-const Users: NextPage = () => {
-  return <MainLayout>Users</MainLayout>;
-};
-
-export default Users;
