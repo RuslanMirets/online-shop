e4992da9dabba508a0670c1953365508879040c6
Ruslan Mirets <ruslan.mirets@gmail.com>
2022-02-18 10:44:07 +0300

    Login, Register

--- server/src/modules/users/users.controller.ts
+++ server/src/modules/users/users.controller.ts
@@ -1,6 +1,6 @@
 import { RolesGuard } from './../../core/guards/roles.guard';
 import { JwtAuthGuard } from './../../core/guards/jwt-auth.guard';
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Roles } from 'src/core/decorators/role-auth.decorator';
 
@@ -13,5 +13,11 @@ export class UsersController {
   @Get()
   getAll() {
     return this.usersService.findAll();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return this.usersService.findOneById(req.user.id);
   }
 }
